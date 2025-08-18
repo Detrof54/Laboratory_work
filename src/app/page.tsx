875ca24cafa71678e24acdca734ef5143cfee616
Михаил Романов <mihailromanov798@gmail.com>
2025-08-18 17:23:06 +0600

    заработал DaisyUI при Tailwind 3 версии

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import {Navbar} from "~/app/_components/navbar"
+import {SigninLink} from "~/app/_components/signlink"
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -14,6 +16,11 @@ export default async function Home() {
 
   return (
     <HydrateClient>
+      <header>
+        {session ? <Navbar session={session}/> : <SigninLink/>}
+      </header>
+
+
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
